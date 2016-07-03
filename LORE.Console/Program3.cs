5d277074b5cc9860c0972c1591c412ae5d19d39c
Tim Goyer <tgoyer@users.noreply.github.com>
2016-07-02 22:05:51 -0500

    Update Program3.cs

--- LORE.Console/Program3.cs
+++ LORE.Console/Program3.cs
@@ -1,1 +1,36 @@
+using LORE.Entities.Characters;
+using LORE.Entities.Items;
+using LORE.Entities.Items.Potions;
+using LORE.Entities.Mechanics.Rules;
+using LORE.Entities.Misc;
+using LORE.MiddeTier;
+using LORE.MiddeTier.Rules;
+using LORE.Console;
+
+namespace LORE.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool loopAgain = true;
+            
+            while(loopAgain)
+            {
+                System.Console.WriteLine("Enter a command: ");
+                var response = System.Console.ReadLine();
+                
+                switch(response) 
+                {
+                    case "i":
+                        ShowInventory();
+                    case "m":
+                        ShowMoney();
+                    case "x":
+                        loopAgain = false;
+                }
+            }
+        }
+    } 
+}
 
